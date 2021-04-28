34291caa7ad6013eb7f0d5b110bf240f77e54395
Robert Mahendra <43535409+uloblonde@users.noreply.github.com>
2021-04-28 08:36:07 +0700

    row & column and parsing data

--- lib/main.dart
+++ lib/main.dart
@@ -11,21 +11,50 @@ class HalamanHalo extends StatelessWidget{
     return new Scaffold(                        //yang memberikan nilai balik pada Scaffold, Scaffold adalah Activity pada flutter
     appBar: new AppBar(
       leading: new Icon(Icons.home),
-    ),
-      body: new Center(
-        child: new Container( // div
-        color: Color.fromRGBO(24, 0, 0, 100),
-         width: 200.0,
-         height: 100.0,
-          child: new Center(
-          child: new Text("Halo", style: new TextStyle(color: Colors.white),
-          ),
-        ),
+      title: new Center(
+        child: new Text("Halo APK")
       ),
+        actions: <Widget>[new Icon(Icons.search)],
+    ), 
+      body: new Container(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch, //ben iso strectch posisine
+          children: <Widget>[
+            new Cardku(icon: Icons.home, teks: "Home", warnaIcon: Colors.cyan,),
+            new Cardku(icon: Icons.local_pizza, teks: "Home", warnaIcon: Colors.black,),
+            new Cardku(icon: Icons.home, teks: "Home", warnaIcon: Colors.brown,),
 
-      ) 
-     
-      
+          ],
+        ),
+        ),
+      );
+  }
+}
+
+class Cardku extends StatelessWidget{
+  
+  Cardku({this.icon,this.teks,this.warnaIcon});
+  final IconData icon;
+  final String teks;
+  final Color warnaIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.all(10.0),
+      child: new Card(
+        child: new Column(
+          children: <Widget>[
+           new Icon(
+             icon, 
+             size: 50.0, 
+             color: warnaIcon,),
+           new Text(
+             teks,
+              style: new TextStyle(fontSize: 20.0),)
+          ],
+        )
+      )
     );
   }
 }
